dafdbb02c0d876e3effa64003d832892ac1dacc7
tomasdisk <tomasdisk@hotmail.com>
2018-04-10 15:13:55 -0300

    render updated

--- render.py
+++ render.py
@@ -2,17 +2,17 @@ import cube as c
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-deepStreng = .4
+deepStreng = -.4
 
 
 def convert_to_2d(p):
-    return c.Point2d(p.cords[0] + (p.cords[2] * deepStreng), p.cords[1] + (p.cords[2] * deepStreng))
+    return c.Point2d(p.cords[0] + (p.cords[1] * deepStreng), p.cords[2] + (p.cords[1] * deepStreng))
 
 
 lis3d = []
-for i in range(-2, 3):
-    lis3d.append([c.Point3d(-2, 2, i), c.Point3d(-2, -2, i)])
-    lis3d.append([c.Point3d(-2, -2, i), c.Point3d(2, -2, -i)])
+lis3d.append([c.Point3d(-3, 0, 0), c.Point3d(3, 0, 0)])
+lis3d.append([c.Point3d(0, 0, -3), c.Point3d(0, 0, 3)])
+lis3d.append([c.Point3d(0, -3, 0), c.Point3d(0, 3, 0)])
 
 for l in lis3d:
     print(l[0].cords, l[1].cords)
@@ -29,6 +29,10 @@ ax = plt.gca()
 for e in lis2d:
     line = mlines.Line2D([e[0].cords[0], e[1].cords[0]], [e[0].cords[1], e[1].cords[1]])
     ax.add_line(line)
+
+p = c.Point3d(0, 3, 0)
+p = convert_to_2d(p)
+plt.scatter(p.cords[0], p.cords[1])
 
 # set limites x e y iguales para el plot
 ax.set_xbound(-5, 5)
