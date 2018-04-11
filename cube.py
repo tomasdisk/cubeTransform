6b150efe8aab0f9a5764fa365deefbaca5a1ee63
tomasdisk <tomasdisk@hotmail.com>
2018-04-11 20:02:41 -0300

    render works fine

--- cube.py
+++ cube.py
@@ -18,57 +18,59 @@ class Point3d:
         self.z = z
         self.cords = (x, y, z)
 
-    def rotate(self, axis, angle, rotCenter):
+    def rotate(self, axis, angle, rot_center):
         # dejo fijo el eje sobre el que se rota
         if axis == 'x':  # hay que fijar x y el plano de rotacion es (y, z)
-            rot_cordU = 1
-            rot_cordV = 2
+            rot_cord_u = 1
+            rot_cord_v = 2
             axis_cord = 0
         elif axis == 'y':  # hay que fijar y y el plano de rotacion es (x, z)
-            rot_cordU = 0
-            rot_cordV = 2
+            rot_cord_u = 0
+            rot_cord_v = 2
             axis_cord = 1
         elif axis == 'z':  # hay que fijar z y el plano de rotacion es (x, y)
-            rot_cordU = 0
-            rot_cordV = 1
+            rot_cord_u = 0
+            rot_cord_v = 1
             axis_cord = 2
+        else:
+            return 0
 
         # guardo valores (u, v) para rotar en el plano y dejo fijo el eje j
-        u = self.cords[rot_cordU]
-        v = self.cords[rot_cordV]
+        u = self.cords[rot_cord_u]
+        v = self.cords[rot_cord_v]
         j = self.cords[axis_cord]
 
         # guardo el centro de rotacion (u, v) en el plano y dejo fijo el eje j
-        rotCenterU = rotCenter.cords[rot_cordU]
-        rotCenterV = rotCenter.cords[rot_cordV]
+        rot_center_u = rot_center.cords[rot_cord_u]
+        rot_center_v = rot_center.cords[rot_cord_v]
 
         s = sin(angle)
         c = cos(angle)
 
         # trasladar el punto al origen
-        u -= rotCenterU
-        v -= rotCenterV
+        u -= rot_center_u
+        v -= rot_center_v
 
         # rotar el punto respecto del origen
         u_new = u * c - v * s
         v_new = u * s + v * c
 
         # trasladar a la pocicion original
-        u = u_new + rotCenterU
-        v = v_new + rotCenterV
+        u = u_new + rot_center_u
+        v = v_new + rot_center_v
 
         # aplico los cambios
-        if rot_cordU == 0:
+        if rot_cord_u == 0:
             self.x = u
-        elif rot_cordU == 1:
+        elif rot_cord_u == 1:
             self.y = u
-        elif rot_cordU == 2:
+        elif rot_cord_u == 2:
             self.z = u
-        if rot_cordV == 0:
+        if rot_cord_v == 0:
             self.x = v
-        elif rot_cordV == 1:
+        elif rot_cord_v == 1:
             self.y = v
-        elif rot_cordV == 2:
+        elif rot_cord_v == 2:
             self.z = v
         if axis_cord == 0:
             self.x = j
@@ -88,13 +90,13 @@ class Face:
         self.p4 = p4
 
     def rotate(self, axis, angle):
-        rotCenter = Point3d(0, 0, 0)
-        self.p1.rotate(axis, angle, rotCenter)
-        self.p2.rotate(axis, angle, rotCenter)
-        self.p3.rotate(axis, angle, rotCenter)
-        self.p4.rotate(axis, angle, rotCenter)
+        rot_center = Point3d(0, 0, 0)
+        self.p1.rotate(axis, angle, rot_center)
+        self.p2.rotate(axis, angle, rot_center)
+        self.p3.rotate(axis, angle, rot_center)
+        self.p4.rotate(axis, angle, rot_center)
 
-    def midPoint(self):
+    def mid_point(self):
         x = (self.p1.x + self.p2.x + self.p3.x + self.p4.x) / 4
         y = (self.p1.y + self.p2.y + self.p3.y + self.p4.y) / 4
         z = (self.p1.z + self.p2.z + self.p3.z + self.p4.z) / 4
@@ -106,21 +108,30 @@ class Cube:
         self.radius = radius
         self.location = location
         self.rotation = rotation
-        self.vertex = [Point3d(self.radius + self.location.x, self.radius + self.location.y, self.radius + self.location.z),
-                       Point3d(self.radius + self.location.x, -self.radius + self.location.y, self.radius + self.location.z),
-                       Point3d(self.radius + self.location.x, -self.radius + self.location.y, -self.radius + self.location.z),
-                       Point3d(self.radius + self.location.x, self.radius + self.location.y, -self.radius + self.location.z),
-                       Point3d(-self.radius + self.location.x, self.radius + self.location.y, self.radius + self.location.z),
-                       Point3d(-self.radius + self.location.x, -self.radius + self.location.y, self.radius + self.location.z),
-                       Point3d(-self.radius + self.location.x, -self.radius + self.location.y, -self.radius + self.location.z),
-                       Point3d(-self.radius + self.location.x, self.radius + self.location.y, -self.radius + self.location.z)]
+        self.vertex = [Point3d(self.radius + self.location.x, self.radius + self.location.y,
+                               self.radius + self.location.z),
+                       Point3d(self.radius + self.location.x, -self.radius + self.location.y,
+                               self.radius + self.location.z),
+                       Point3d(self.radius + self.location.x, -self.radius + self.location.y,
+                               -self.radius + self.location.z),
+                       Point3d(self.radius + self.location.x, self.radius + self.location.y,
+                               -self.radius + self.location.z),
+                       Point3d(-self.radius + self.location.x, self.radius + self.location.y,
+                               self.radius + self.location.z),
+                       Point3d(-self.radius + self.location.x, -self.radius + self.location.y,
+                               self.radius + self.location.z),
+                       Point3d(-self.radius + self.location.x, -self.radius + self.location.y,
+                               -self.radius + self.location.z),
+                       Point3d(-self.radius + self.location.x, self.radius + self.location.y,
+                               -self.radius + self.location.z)]
         self.f1 = Face(self.vertex[0], self.vertex[1], self.vertex[2], self.vertex[3])  # fijo x
         self.f2 = Face(self.vertex[4], self.vertex[5], self.vertex[6], self.vertex[7])  # fijo -x
         self.f3 = Face(self.vertex[0], self.vertex[4], self.vertex[7], self.vertex[3])  # fijo y
         self.f4 = Face(self.vertex[1], self.vertex[5], self.vertex[6], self.vertex[2])  # fijo -y
         self.f5 = Face(self.vertex[0], self.vertex[4], self.vertex[5], self.vertex[1])  # fijo z
         self.f6 = Face(self.vertex[3], self.vertex[7], self.vertex[6], self.vertex[2])  # fijo -z
-        # los edges son la union de cada punto de la cara f1 y f2 con el punto que le sigue de la misma cara y las uniones entre puntos iguales entre caras
+        # los edges son la union de cada punto de la cara f1 y f2 con el punto que le sigue de la misma cara
+        # y las uniones entre puntos iguales entre caras
         self.edges = [(self.f1.p1, self.f1.p2),
                       (self.f1.p2, self.f1.p3),
                       (self.f1.p3, self.f1.p4),
@@ -135,59 +146,63 @@ class Cube:
                       (self.f1.p4, self.f2.p4)]
 
     def rotate(self, axis, angle):
-        rotCenter = self.location
+        rot_center = self.location
         for v in self.vertex:
-            v.rotate(axis, angle, rotCenter)
+            v.rotate(axis, angle, rot_center)
 
-    def proyectVertex(self, axis):
+    def proyect_vertex(self, axis):
         if axis == 'x':  # el resultado es un punto proyectado (y, z)
-            rot_cordU = 1
-            rot_cordV = 2
+            rot_cord_u = 1
+            rot_cord_v = 2
         elif axis == 'y':  # el resultado es un punto proyectado (x, z)
-            rot_cordU = 0
-            rot_cordV = 2
+            rot_cord_u = 0
+            rot_cord_v = 2
         elif axis == 'z':  # el resultado es un punto proyectado (x, y)
-            rot_cordU = 0
-            rot_cordV = 1
+            rot_cord_u = 0
+            rot_cord_v = 1
+        else:
+            return 0
 
         # devuelvo un lista de Point2d proyectados en el plano axis
         lis = []
         for v in self.vertex:
-            lis.append(Point2d(v.cords[rot_cordU], v.cords[rot_cordV]))
+            lis.append(Point2d(v.cords[rot_cord_u], v.cords[rot_cord_v]))
         return lis
 
-    def proyectEdges(self, axis):
+    def proyect_edges(self, axis):
         if axis == 'x':  # el resultado es un punto proyectado (y, z)
-            rot_cordU = 1
-            rot_cordV = 2
+            rot_cord_u = 1
+            rot_cord_v = 2
         elif axis == 'y':  # el resultado es un punto proyectado (x, z)
-            rot_cordU = 0
-            rot_cordV = 2
+            rot_cord_u = 0
+            rot_cord_v = 2
         elif axis == 'z':  # el resultado es un punto proyectado (x, y)
-            rot_cordU = 0
-            rot_cordV = 1
+            rot_cord_u = 0
+            rot_cord_v = 1
+        else:
+            return 0
 
         # devuelvo un lista de pares de Point2d proyectados en el plano axis
         lis = []
         for e in self.edges:
-            lis.append((Point2d(e[0].cords[rot_cordU], e[0].cords[rot_cordV]),
-                        Point2d(e[1].cords[rot_cordU], e[1].cords[rot_cordV])))
+            lis.append((Point2d(e[0].cords[rot_cord_u], e[0].cords[rot_cord_v]),
+                        Point2d(e[1].cords[rot_cord_u], e[1].cords[rot_cord_v])))
         return lis
 
-    def proyectPerimeter(self, axis):
+    def proyect_perimeter(self, axis):
         # lis = self.proyectVertex(axis)
         # TODO quedarme con los Point2d en la lista que pertencen al perimetro
         pass
 
-    def shadowArea(self, axis):
+    def shadow_area(self, axis):
         # lis = self.proyectPerimeter(axis)
         pass
 
-    def plotPointsProyection(self, axis):
+    def plot_points_proyection(self, axis):
         import matplotlib.pyplot as plt
         x = []
         y = []
-        for p in self.proyectVertex(axis):
+        for p in self.proyect_vertex(axis):
             x.append(p.cords[0])
             y.append(p.cords[1])
 
@@ -202,14 +217,14 @@ class Cube:
 
         plt.show()
 
-    def plotCubeProyection(self, axis):
+    def plot_cube_proyection(self, axis):
         import matplotlib.pyplot as plt
         import matplotlib.lines as mlines
         ax = plt.gca()
-        for e in self.proyectEdges(axis):
+        for e in self.proyect_edges(axis):
             line = mlines.Line2D([e[0].cords[0], e[1].cords[0]], [e[0].cords[1], e[1].cords[1]])
             ax.add_line(line)
-        for v in self.proyectVertex(axis):
+        for v in self.proyect_vertex(axis):
             plt.scatter(v.cords[0], v.cords[1], c='b')
 
         # set limites x e y iguales para el plot
@@ -224,10 +239,10 @@ class Cube:
 if __name__ == "__main__":
     C = Cube()
     print("Se lo proyecta el cubo sobre el eje Z.")
-    C.plotCubeProyection('z')
+    C.plot_cube_proyection('z')
     print("Se rota el cubo 2 radianes en el eje Y, luego se lo proyecta sobre Z.")
     C.rotate('y', 2)
-    C.plotCubeProyection('z')
+    C.plot_cube_proyection('z')
     print("Se vuelver a rotar el cubo 2 radianes en el eje X, luego se lo vuelve proyectar sobre Z.")
     C.rotate('x', 2)
-    C.plotCubeProyection('z')
+    C.plot_cube_proyection('z')
--- render.py
+++ render.py
@@ -2,40 +2,80 @@ import cube as c
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
-deepStreng = -.4
+deep_x_streng = -.6
+deep_y_streng = -.5
+distance = 10
 
 
-def convert_to_2d(p):
-    return c.Point2d(p.cords[0] + (p.cords[1] * deepStreng), p.cords[2] + (p.cords[1] * deepStreng))
+def convert_to_2d(point):
+    return c.Point2d(point.cords[0] + (point.cords[1] * deep_x_streng), point.cords[2] + (point.cords[1] * deep_y_streng))
 
 
-lis3d = []
-lis3d.append([c.Point3d(-3, 0, 0), c.Point3d(3, 0, 0)])
-lis3d.append([c.Point3d(0, 0, -3), c.Point3d(0, 0, 3)])
-lis3d.append([c.Point3d(0, -3, 0), c.Point3d(0, 3, 0)])
-
-for l in lis3d:
-    print(l[0].cords, l[1].cords)
+lis3d = [  # ejes
+         [c.Point3d(-distance, 0, 0), c.Point3d(distance, 0, 0)],
+         [c.Point3d(0, 0, -distance), c.Point3d(0, 0, distance)],
+         [c.Point3d(0, -distance, 0), c.Point3d(0, distance, 0)],
+         # lines de referencia segun 'distace'
+         [c.Point3d(-distance, -distance, -distance), c.Point3d(-distance, distance, -distance)],
+         [c.Point3d(-distance, -distance, -distance), c.Point3d(distance, -distance, -distance)],
+         [c.Point3d(-distance, -distance, distance), c.Point3d(-distance, distance, distance)],
+         [c.Point3d(-distance, -distance, distance), c.Point3d(distance, -distance, distance)],
+         [c.Point3d(-distance, distance, distance), c.Point3d(-distance, distance, -distance)],
+         [c.Point3d(-distance, -distance, distance), c.Point3d(-distance, -distance, -distance)],
+         [c.Point3d(distance, -distance, distance), c.Point3d(distance, -distance, -distance)],
+         [c.Point3d(-distance, distance, -distance), c.Point3d(distance, distance, -distance)],
+         [c.Point3d(distance, distance, -distance), c.Point3d(distance, -distance, -distance)]]
 
 lis2d = []
 for l in lis3d:
     lis2d.append([convert_to_2d(l[0]), convert_to_2d(l[1])])
 
-for l in lis2d:
-    print(l[0].cords, l[1].cords)
-
+C = c.Cube(radius=2)
+C.rotate('y', 2)
+C.rotate('x', 2)
+for l in C.edges:
+    lis2d.append([convert_to_2d(l[0]), convert_to_2d(l[1])])
+    lis2d.append([convert_to_2d(c.Point3d(-distance, l[0].cords[1], l[0].cords[2])),
+                  convert_to_2d(c.Point3d(-distance, l[1].cords[1], l[1].cords[2]))])
+    lis2d.append([convert_to_2d(c.Point3d(l[0].cords[0], -distance, l[0].cords[2])),
+                  convert_to_2d(c.Point3d(l[1].cords[0], -distance, l[1].cords[2]))])
+    lis2d.append([convert_to_2d(c.Point3d(l[0].cords[0], l[0].cords[1], -distance)),
+                  convert_to_2d(c.Point3d(l[1].cords[0], l[1].cords[1], -distance))])
 
 ax = plt.gca()
 for e in lis2d:
     line = mlines.Line2D([e[0].cords[0], e[1].cords[0]], [e[0].cords[1], e[1].cords[1]])
     ax.add_line(line)
 
-p = c.Point3d(0, 3, 0)
-p = convert_to_2d(p)
-plt.scatter(p.cords[0], p.cords[1])
+# p = c.Point3d(0, 0, 3)
+# p = convert_to_2d(p)
+# plt.scatter(p.cords[0], p.cords[1])
+
+x = []
+y = []
+for v in C.vertex:
+    # proyect x
+    point = c.Point3d(-distance, v.cords[1], v.cords[2])
+    p = convert_to_2d(point)
+    x.append(p.cords[0])
+    y.append(p.cords[1])
+    # proyect y
+    point = c.Point3d(v.cords[0], -distance, v.cords[2])
+    p = convert_to_2d(point)
+    x.append(p.cords[0])
+    y.append(p.cords[1])
+    # proyect z
+    point = c.Point3d(v.cords[0], v.cords[1], -distance)
+    p = convert_to_2d(point)
+    x.append(p.cords[0])
+    y.append(p.cords[1])
+
+
+plt.scatter(x, y)
 
 # set limites x e y iguales para el plot
-ax.set_xbound(-5, 5)
-ax.set_ybound(-5, 5)
+limit = distance * 2
+ax.set_xbound(-limit, limit)
+ax.set_ybound(-limit, limit)
 
 plt.show()
